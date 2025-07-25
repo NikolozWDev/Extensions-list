d1a37df06659e7d2798b6f9e2ef64fa76334cbb9
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-07-25 11:05:04 +0400

    git init

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  darkMode: 'class',
+  theme: {
+
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      xxl: '1536px',
+    },
+
+    fontFamily: {
+      noto: ['"Noto Sans"', 'sans-serif'],
+    },
+
+    extend: {
+      colors: {
+        dneu9: 'hsl(227, 75%, 14%)',
+        dneu8: 'hsl(226, 25%, 17%)',
+        dneu7: 'hsl(225, 23%, 24%)',
+        dneu6: 'hsl(226, 11%, 37%)',
+
+        lneu5: 'hsl(0, 0%, 78%)',
+        lneu4: 'hsl(217, 61%, 90%)',
+        lneu3: 'hsl(0, 0%, 93%)',
+        lneu2: 'hsl(200, 60%, 99%)',
+      }
+    },
+
+
+  },
+  plugins: [],
+}
